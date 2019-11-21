020b95b96331d32b4f3337a0e1d756def5768e7d
DESKTOP-ECAGOK7\johnentrieri <john.entrieri@gmail.com>
2019-11-20 22:59:29 -0500

    Created Layout and Aux Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <h1>Burger Builder</h1>
+      <Layout>
+        <h1>Burger Builder</h1>
+      </Layout>
     </div>
   );
 }
